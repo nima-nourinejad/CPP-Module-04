bcb8eb8b6e8e1436edb122f57f43ca687fa91e61
Nima Nourinejad <nima.nourinejad@gmail.com>
2024-09-06 14:35:35 +0300

    update

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -7,34 +7,25 @@
 #include <iostream>
 
 int main() {
-  Cat cat_1;
-  cat_1.setBrainIdea("Cat idea", 0);
-  std::cout << cat_1.getIdea(0) << std::endl;
-  std::cout << std::endl;
+	Animal **animals = new Animal*[4];
+	animals[0] = new Cat();
+	animals[1] = new Cat();
+	animals[2] = new Dog();
+	animals[3] = new Dog();
+	for (int i = 0; i < 4; i++)
+		animals[i]->makeSound();
+	for (int i = 0; i < 4; i++)
+		delete animals[i];
+	delete[] animals;
 
-  Cat cat_2 = cat_1;
-  std::cout << cat_2.getIdea(0) << std::endl;
-  std::cout << std::endl;
+	std::cout << "---------------------" << std::endl;
+	Cat *cat = new Cat();
+	cat->setBrainIdea("I am a cat", 0);
+	Cat copy(*cat);
+	delete cat;
+	std::cout << copy.getIdea(0) << std::endl;
 
-  Dog dog_1;
-  dog_1.setBrainIdea("Dog idea", 0);
-  std::cout << dog_1.getIdea(0) << std::endl;
-  std::cout << std::endl;
 
-  Dog *dog_2 = new Dog(dog_1);
-  std::cout << dog_2->getIdea(0) << std::endl;
-  delete dog_2;
-  std::cout << std::endl;
-  std::cout << std::endl;
-
-  const Animal *j = new Dog();
-  delete j;
-  std::cout << std::endl;
-  std::cout << std::endl;
-  
-  const Animal *i = new Cat();
-  delete i;
-  std::cout << std::endl;
-  std::cout << std::endl;
+	
   return 0;
 }
